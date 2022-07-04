660996a1ee5190bbaaf73d9e2feac4ff929e8788
Davide <me@davegabe.it>
2022-07-04 15:07:11 +0200

    Corrupter now draws using more fantasy

--- data/corrupter.py
+++ data/corrupter.py
@@ -38,6 +38,48 @@ def corrupt(training_path: str, evaluation_path: str, training_corrupted_path: s
             cv.imwrite(final_path, img)
 
 
+def drawCurve(img: cv.Mat, start_point: tuple):
+    """
+        Draws a curve on the image starting from one point and moving to near points on the image.
+
+        Args:
+            img: The image to draw the curve on.
+            DEBUG: Whether or not to print debug messages.
+    """
+    x = img.shape[0]
+    y = img.shape[1]
+    thickness = int(0.035 * min(x, y))
+    points = [start_point]
+    minPoints = 20
+    maxPoints = 50
+
+    # random direction tuple in [-1, 1]
+    direction = ((random.random()-0.5)*2, (random.random()-0.5)*2)
+    # list of random points to draw
+    for i in range(minPoints, maxPoints):
+        # get a random point near the last point in the list
+        x1 = int(points[-1][0] + direction[0] * thickness)
+        y1 = int(points[-1][1] + direction[1] * thickness)
+
+        # clamp the point to the image
+        x1 = max(0, min(x1, x-1))
+        y1 = max(0, min(y1, y-1))
+
+        # get next direction "human-style" (trying to not completely inverse it)
+        while True:
+            new_direction = ((random.random()-0.5)*2, (random.random()-0.5)*2) # get a new random direction in [-1, 1]
+            if abs(new_direction[0]-direction[0]) < 0.7 and abs(new_direction[1]-direction[1]) < 0.7: 
+                direction = new_direction
+                break
+        
+        # add the point to the list
+        points.append((x1, y1))
+
+    # draw line passing through points
+    for i in range(1, len(points)):
+        cv.line(img, points[i-1], points[i], 0, thickness)
+
+
 def draw(img: cv.Mat, DEBUG=False):
     """
         Draws a random line on the image.
@@ -47,16 +89,12 @@ def draw(img: cv.Mat, DEBUG=False):
     """
     x = img.shape[0]
     y = img.shape[1]
-    minPoints = 3
-    maxPoints = 10
+    minCurves = 1
+    maxCurves = 4
 
-    # list of random points to draw
-    points = [(random.randint(0, x), random.randint(0, y))
-              for i in range(minPoints, maxPoints)]
-
-    # draw line passing through points
-    for i in range(1, len(points)):
-        cv.line(img, points[i-1], points[i], 0, int(0.035 * min(x, y))) # thickness of line based on image size
+    # draw random number of curves
+    for i in range(random.randint(minCurves, maxCurves)):
+        drawCurve(img, (random.randint(0, x), random.randint(0, y)))
 
     if (DEBUG == True):
         cv.imshow("img", img)
